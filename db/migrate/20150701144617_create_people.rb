acbf9947386f78e5b2b6aac4523e2efa3cd38662
Marco <marcooliveira@infojr.com.br>
2015-07-01 13:32:35 -0300

    Utilizado um pouco do ActiveRecords

--- db/migrate/20150701144617_create_people.rb
+++ db/migrate/20150701144617_create_people.rb
@@ -1,11 +1,12 @@
 class CreatePeople < ActiveRecord::Migration
   def change
     create_table :people do |t|
-      t.string :name
-      t.string :email
-      t.string :password
+      t.string :name, limit: 50, null: false
+      t.string :email, limit: 100
+      t.string :password, limit: 50
       t.date :born_at
       t.boolean :admin
+      t.index :email, unique: true
 
       t.timestamps null: false
     end
