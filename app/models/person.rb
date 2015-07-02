e592802cd19cba7c90d65faf808f46bd73e57c75
Marco <marcooliveira@infojr.com.br>
2015-07-02 12:43:45 -0300

    Criado comportamentos para os testes, criado atributo e callback no modelo para guardar senha criptografada e criptografar senha antes de salva-la, respectivamente.

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,11 +1,24 @@
+require 'digest/sha1'
 class Person < ActiveRecord::Base
 	validates :name, presence: true, length: {maximum: 50}
 	validates :email, allow_blank: true, allow_nil: true, uniqueness: true, format: {with: /\A[a-zA-Z0-9_.-]+@([a-zA-Z0-9_-]+\.)+[a-zA-Z]{2,4}\z/}
 	validates :born_at, presence: true
 	validate :age_limit
 
+	attr_accessor :plain_password
+	before_save :encrypt_password
+
+	def self.encrypt_password(pwd)
+		Digest::SHA1.hexdigest("123_#{pwd}_456")
+	end
+
 	private
 	def age_limit
 		errors.add(:born_at, "tem que ser maior que 16 anos") if self.born_at.nil? || Date.today.year-self.born_at.year < 16
+	end
+
+	def encrypt_password
+		return if plain_password.to_s.strip.size < 1
+		self.password = Person.encrypt_password(plain_password)
 	end
 end
--- test/models/person_test.rb
+++ test/models/person_test.rb
@@ -44,4 +44,31 @@ class PersonTest < ActiveSupport::TestCase
   	assert @person.valid? 
   end
 
+  test "tem que retornar a senha criptografada correta" do
+  	assert_equal Digest::SHA1.hexdigest("123_test_456"), Person.encrypt_password("test")
+  end
+
+  test "tem que ter atributo para receber a senha em texto puro" do
+  	assert_respond_to @person, :plain_password=
+  end
+
+  test "tem que ter atributo para retornar a senha em texto puro como vazia" do
+  	assert_respond_to @person, :plain_password
+  	assert_nil @person.plain_password
+  end
+  	
+  test "tem que gravar a senha encriptografada no atributo password" do
+  	@person.plain_password = "teste"
+  	@person.save
+  	assert_equal Person.encrypt_password("teste"), @person.password
+  end
+
+  test "nao deve alterar a senha encriptografada se enviada senha em texto puro vazia" do
+  	@person.plain_password = "teste"
+  	old_encrypted = @person.password
+  	@person.plain_password = ""
+  	@person.save
+  	assert_equal old_encrypted, @person.password
+  end
+  
 end
