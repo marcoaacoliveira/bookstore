44425e5c61bc73a860477fe5282b1ecd055aef6c
marcoaacoliveira <marcoaacoliveira@gmail.com>
2015-07-08 14:57:06 -0300

    Criado o método para login e o método para logout.

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -1,7 +1,40 @@
 class LoginController < ApplicationController
   def login
+    if request.post?
+      email = params[:email]
+      password = params[:password]
+      if email.blank? && password.blank?
+        flash[:notice] = "Informe email e senha"
+        return
+      end
+
+      if email.blank?
+        flash[:notice] = "Informe o email"
+        return
+      end
+
+      if password.blank?
+        flash[:notice] = "Informe a senha"
+      end
+
+      person = Person.auth(email,password)
+      if !person
+        flash[:notice] = "Falha no login"
+        return
+      end
+
+      flash[:notice] = "Bem-vindo, #{person.name}!"
+      session[:id] = person.id
+      session[:name] = person.name
+      session[:admin] = person.admin
+      redirect_to people_path
+    end
   end
 
   def logout
+    session[:id] = nil
+    session[:name] = nil
+    session[:admin] = nil
+    redirect_to action: :login
   end
 end
