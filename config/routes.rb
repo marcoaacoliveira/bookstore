5a593e30443400324157c7ce50fa7225f4e71204
Marco <marcooliveira@infojr.com.br>
2015-07-15 11:11:03 -0300

    Adicionado rota para o changed e view.

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,9 @@ Rails.application.routes.draw do
     collection do
       get :admins
     end
+    member do
+    	get :changed
+    end
   end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
